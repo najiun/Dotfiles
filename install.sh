21677a6d526304547a471d6697d8dc599a7fdef8
najiun <lamtic@proton.me>
2025-08-03 13:13:32 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bash/bin
+#!/bin/bash
 
 set -e
 
@@ -14,9 +14,7 @@ for package in "${packages[@]}"; do
 	
 done
 
-mkdir /home/user/Applications
-mkdir /home/user/Applications/Bitwarden
-mkdir /home/user/Applications/Obsidian
+mkdir -p "$HOME/Applications/Bitwarden" "$HOME/Applications/Obsidian"
 
 #Brave Browser installation
 
@@ -33,32 +31,35 @@ sudo apt install brave-browser
 #Bitwarden installation
 
 curl https://bitwarden.com/download/?app=desktop&platform=linux&variant=appimage -o /home/user/Applications/Bitwarden/bitwarden.Appimage
-sudo chmod 755 bitwarden.Appimage
+chmod 755 "$HOME/Applications/Bitwarden/bitwarden.Appimage"
 sudo curl blob:https://github.com/4982141e-3f78-4694-97e0-464dcfea6612 -o /home/user/Applications/Bitwarden/logo.png
 touch /home/user/.local/share/applications/bitwarden.desktop
-echo >> [Desktop Entry]
+cat > /home/user/.local/share/applications/bitwarden.desktop << EOF
+[Desktop Entry]
 Name:Bitwarden
 Exec:/home/user/Applications/Bitwarden/bitwarden.Appimage
 Logo:/home/user/Applications/Bitwarden/logo.png
 Type:Application
+EOF
 
->> /home/user/.local/share/applications/bitwarden.desktop
+sudo update-desktop-database
 
-update-desktop-database ~/.local/share/applications
 
 #Obsidian installation
 sudo curl https://github.com/obsidianmd/obsidian-releases/releases/download/v1.8.10/Obsidian-1.8.10.AppImage -o /home/user/Applications/Obsidian/obsidian.Appimage
-sudo chmod 755 obsidian.Appimage
+chmod 755 "$HOME/Applications/Obsidian/obsidian.Appimage"
 sudo curl https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/2023_Obsidian_logo.svg/250px-2023_Obsidian_logo.svg.png -o /home/user/Applications/Obsidian/logo.png
 touch /home/user/.local/share/applications/obsidian.desktop
-echo >> [Desktop Entry]
+cat > /home/user/.local/share/applications/obsidian.desktop << EOF
+[Desktop Entry]
 Name:Obsidian
 Exec:/home/user/Applications/Obsidian/obsidian.Appimage
 Logo:/home/user/Applications/Obsidian/logo.png
 Type:Application
->> /home/user/.local/share/applications/obsidian.desktop
+EOF
 
-update-desktop-database ~/.local/share/applications
+sudo update-desktop-database
+
 
 #hBlock installion(set-up after system config)
 curl -o /user/hblock 'https://raw.githubusercontent.com/hectorm/hblock/v3.5.1/hblock' \
@@ -76,7 +77,6 @@ curl -o /user/hblock 'https://raw.githubusercontent.com/hectorm/hblock/v3.5.1/hb
 
 
 #UFW CONFIG
-ufw enable
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo ufw allow ssh
@@ -85,6 +85,7 @@ sudo ufw allow out 443 #HTTP/HTTPS
 sudo ufw allow 21 #FTP
 sudo ufw allow 25 #Email
 sudo ufw allow 53 #DNS
+ufw enable
 
 #hblock
 sudo curl https://raw.githubusercontent.com/najiun/Dotfiles/refs/heads/main/my-sources.list -o /home/user/my-sources.list
